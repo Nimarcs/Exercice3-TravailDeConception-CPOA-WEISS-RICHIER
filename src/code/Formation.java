9e33f6f13a60dcf95a6df98325d8ece6bb11e36a
Nimarcs <moimonautremoi@gmail.com>
2021-09-22 11:22:37 +0200

    Finalisation de formation
    Ajout des methodes manquante de formation

--- src/code/Formation.java
+++ src/code/Formation.java
@@ -1,9 +1,11 @@
 package code;
 
 import exceptions.MatiereExisteDejaException;
+import exceptions.MatiereInexistanteException;
 import exceptions.ValeurNegativeImpossibleException;
 
 import java.util.HashMap;
+import java.util.Set;
 
 public class Formation {
 
@@ -54,8 +56,48 @@ public class Formation {
     public void supprimerMatiere(String matiere){
         coefficients.remove(matiere);
     }
-    
 
+    /**
+     * methode qui permet de modifier le coefficient d'une matiere
+     * @param matiere matiere dont on veut changer le coefficient, doit deja exister
+     * @param coeff coefficient a attribuer a la matiere, doit etre positif
+     * @throws ValeurNegativeImpossibleException renvoye si le coefficient est negatif
+     * @throws MatiereInexistanteException renvoye si la matiere n'existe pas
+     */
+    public void changerCoefficient(String matiere, Double coeff) throws ValeurNegativeImpossibleException, MatiereInexistanteException {
+        if (coeff < 0)
+            throw new ValeurNegativeImpossibleException(coeff);
+        if (!coefficients.containsKey(matiere)){
+            throw new MatiereInexistanteException(matiere);
+        }
+        coefficients.replace(matiere, coeff);
+    }
 
+    /**
+     * methode permettant de recuperer le domaine des matieres
+     * @return set de matiere
+     */
+    public Set<String> domaineMatieres(){
+        return coefficients.keySet();
+    }
 
+    /**
+     * methode qui permet de recuperer le coefficient d'une matiere
+     * @param matiere matiere sont on veut le coefficient, doit etre dans la formation
+     * @return coefficient de la matiere
+     * @throws MatiereInexistanteException renvoye si la matiere n'existe pas
+     */
+    public Double getCoefficient(String matiere) throws MatiereInexistanteException {
+        if (!coefficients.containsKey(matiere))
+            throw new MatiereInexistanteException(matiere);
+        return coefficients.get(matiere);
+    }
+
+    /**
+     * getter de idFormation
+     * @return identifiant de la formation
+     */
+    public String getIdFormation() {
+        return idFormation;
+    }
 }
