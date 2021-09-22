e3ad71b4b7f7cb5b3495d78fa091cab0688cfdb9
Nimarcs <moimonautremoi@gmail.com>
2021-09-22 10:58:18 +0200

    Ajout de javadoc pour l'exception MatiereExisteDeja

--- src/exceptions/MatiereExisteDejaException.java
+++ src/exceptions/MatiereExisteDejaException.java
@@ -2,6 +2,10 @@ package exceptions;
 
 public class MatiereExisteDejaException extends Exception{
 
+    /**
+     * exception appelle lorsque l'on donne une matiere qui existe deja alors que l'on attend une valeur nouvelle
+     * @param nom nom de la matiere
+     */
     public MatiereExisteDejaException(String nom){
         super("La matiere " + nom + " existe deja");
     }
