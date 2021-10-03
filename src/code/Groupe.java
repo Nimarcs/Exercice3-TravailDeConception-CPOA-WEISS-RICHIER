7ad3c44da6e38fc6a550711c9397dab3f0bee617
weiss54 <72012859+weiss54@users.noreply.github.com>
2021-10-03 12:13:58 +0200

    ajout des methodes vides et constructeurs de la classe Groupe

--- src/code/Groupe.java
+++ src/code/Groupe.java
@@ -1,12 +1,51 @@
 // PACKAGE
 package code;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * CLASSE GROUPE
  * Definit une groupe d'etudiant dans un domaine de formation
  */
 public class Groupe {
 
-    
+    // ATTRIBUTS
+    private Set<Etudiant> etudiants;
+    private Formation formation;
+
+    // CONSTRUCTEUR
+    public Groupe(Formation form) {
+        this.formation = form;
+        this.etudiants = new HashSet<Etudiant>();
+    }
+
+    // METHODES
+    public void ajouterEtudiant(Etudiant etu) {
+
+    }
+
+    public void supprimerEtudiant(Etudiant etu) {
+
+    }
+
+    public double calculerMoyenneGroupe(String matiere) {
+
+    }
+
+    public double calculerMoyenneGenerale() {
+
+    }
+
+    public void triParMerite() {
+
+    }
+
+    public void triAlpha() {
+        
+    }
+
 
 }
