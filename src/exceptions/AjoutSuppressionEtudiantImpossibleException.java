a11e45883b0861ad13181d9a2e67dc877532509f
weiss54 <72012859+weiss54@users.noreply.github.com>
2021-10-06 13:00:55 +0200

    modification syntaxique de l'exception AjoutSupressionImpossible

--- src/exceptions/AjoutSuppressionEtudiantImpossibleException.java
+++ src/exceptions/AjoutSuppressionEtudiantImpossibleException.java
@@ -6,7 +6,7 @@ import code.Groupe;
 public class AjoutSuppressionEtudiantImpossibleException extends Exception {
 
     public AjoutSuppressionEtudiantImpossibleException(Etudiant e){
-        super("L'ajout de l'etudiant"+e.getIdentite().getNom()+" "+e.getIdentite().getPrenom()+" ne peux se faire dans ce groupe car il ne suit pas la même formation");
+        super("L'operation d'ajout ou de suppresion de l'etudiant "+e.getIdentite().getNom()+" "+e.getIdentite().getPrenom()+" n'a pas pu se faire");
     }
 
 }
