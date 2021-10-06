59a41929856b6d6d8c9cb191d23dec15a0645698
Nimarcs <moimonautremoi@gmail.com>
2021-10-06 13:15:47 +0200

    Ajout des tests d'Etudiants

--- src/tests/TestsEtudiants.java
+++ src/tests/TestsEtudiants.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import code.Etudiant;
 import code.Formation;
 import code.Identite;
+import exceptions.ListeNotesVideException;
 import exceptions.MatiereInexistanteException;
 import exceptions.ValeurImpossibleException;
 
@@ -55,7 +56,7 @@ public class TestsEtudiants {
     /**
      * le test verifie que la methode renvoie bien une exception si la matiere n'existe pas
      * @throws MatiereInexistanteException exception attendue
-     * @throws ValeurImpossibleException
+     * @throws ValeurImpossibleException exception non attendue
      */
     @Test (expected = MatiereInexistanteException.class)
     public void ajouterNote_casMatiereInexistante() throws MatiereInexistanteException, ValeurImpossibleException {
@@ -64,32 +65,192 @@ public class TestsEtudiants {
         etu1.ajouterNote("informatique", 20.0);
         //assertion
     }
-    
-    @Test
-    public void ajouterNote_casNegatif() {
+
+    /**
+     * le test verifie que la methode renvoie bien une exception si la note est negative
+     * @throws MatiereInexistanteException exception non attendue
+     * @throws ValeurImpossibleException exception attendue
+     */
+    @Test (expected = ValeurImpossibleException.class)
+    public void ajouterNote_casNegatif() throws MatiereInexistanteException, ValeurImpossibleException {
         //preparation
         //methode a tester
+        etu1.ajouterNote("epluchage", -1.5);
         //assertion
     }
 
-    @Test
-    public void ajouterNote_casGrandeValeur() {
+    /**
+     * le test verifie que la methode renvoie bien une exception si la note est superieure a 20
+     * @throws MatiereInexistanteException exception non attendue
+     * @throws ValeurImpossibleException exception attendue
+     */
+    @Test (expected = ValeurImpossibleException.class)
+    public void ajouterNote_casGrandeValeur() throws MatiereInexistanteException, ValeurImpossibleException {
         //preparation
         //methode a tester
+        etu1.ajouterNote("epluchage", 20.2);
         //assertion
     }
 
-    
-    
+
+    /**
+     * test qui verifie le bon fonctionnement de calculer moyenne dans un cas normal
+     * @throws MatiereInexistanteException exception non attendue
+     * @throws ValeurImpossibleException exception non attendue
+     * @throws ListeNotesVideException exception non attendue
+     */
     @Test
-    public void calculerMoyenne() {
+    public void calculerMoyenne_casNormal() throws MatiereInexistanteException, ValeurImpossibleException, ListeNotesVideException {
+        //preparation
+        etu1.ajouterNote("cueille", 3.0);
+        etu1.ajouterNote("cueille", 7.0);
+        etu1.ajouterNote("cueille", 20.0);
+        double exp = 10.0;
+        //methode a tester
+        double res = etu1.calculerMoyenne("cueille");
+        //assertion
+        assertEquals("l'etudiant doit avoir la bonne moyenne", exp, res, 0.01);
     }
 
-    @Test
-    public void calculerMoyenneGenerale() {
+    /**
+     * test qui verifie que la methode calculerMoyenne renvoie une exception si on essaie de calculer la moyenne dans une matiere inexistante
+     * @throws MatiereInexistanteException exception attendue
+     * @throws ListeNotesVideException exception non attendue
+     */
+    @Test (expected = MatiereInexistanteException.class)
+    public void calculerMoyenne_casMatiereInexistante() throws MatiereInexistanteException, ListeNotesVideException {
+        //preparation
+        //methode a tester
+        etu1.calculerMoyenne("lancer");
+        //assertion
     }
 
-    @Test
-    public void getIdentite() {
+    /**
+     * test qui permet de verifier si calculer moyenne renvoie bien une exception si il essaie de calculer la moyenne d'une matiere sans note
+     * @throws MatiereInexistanteException exception non attendue
+     * @throws ListeNotesVideException exception attendue
+     */
+    @Test (expected = ListeNotesVideException.class)
+    public void calculerMoyenne_casPasDeNote() throws MatiereInexistanteException, ListeNotesVideException {
+        //preparation
+        //methode a tester
+        etu1.calculerMoyenne("epluchage");
+        //assertion
     }
+
+    /**
+     * test qui permet de tester le cas ou il y a une infinite de chiffre après la virgule
+     * @throws MatiereInexistanteException exception non attendue
+     * @throws ValeurImpossibleException exception non attendue
+     * @throws ListeNotesVideException exception non attendue
+     */
+    @Test
+    public void calculerMoyenne_casMoyenneAvecVirguleInfinie() throws MatiereInexistanteException, ValeurImpossibleException, ListeNotesVideException {
+        //preparation
+        etu1.ajouterNote("cueille", 2.0);
+        etu1.ajouterNote("cueille", 2.0);
+        etu1.ajouterNote("cueille", 1.0);
+        double exp = 1.66;
+        //methode a tester
+        double res = etu1.calculerMoyenne("cueille");
+        //assertion
+        assertEquals("l'etudiant doit avoir la bonne moyenne", exp, res, 0.01);
+    }
+
+    /**
+     * test qui permet de verifier que le calcul de la moyenne generale fonctionne dans un cas normal
+     * @throws MatiereInexistanteException exception non attendue
+     * @throws ValeurImpossibleException exception non attendue
+     * @throws ListeNotesVideException exception non attendue
+     */
+    @Test
+    public void calculerMoyenneGenerale_casNormal() throws MatiereInexistanteException, ValeurImpossibleException, ListeNotesVideException {
+        //preparation
+        etu1.ajouterNote("cueille", 5.0);
+        etu1.ajouterNote("cueille", 15.0);
+        etu1.ajouterNote("epluchage", 0.0);
+        etu1.ajouterNote("cuisson", 10.0);
+        etu1.ajouterNote("cuisson", 20.0);
+        double exp = 5.0;
+        //methode a tester
+        double res = etu1.calculerMoyenneGenerale();
+        //assertion
+        assertEquals("l'etudiant doit avoir la bonne moyenne", exp, res, 0.01);
+    }
+
+    /**
+     * test pour le cas ou il n'y a pas de note dans une matiere
+     * @throws MatiereInexistanteException exception non attendue
+     * @throws ValeurImpossibleException exception non attendue
+     */
+    @Test
+    public void calculerMoyenneGenerale_casPasNoteDansMatiere() throws MatiereInexistanteException, ValeurImpossibleException, ListeNotesVideException {
+        //preparation
+        etu1.ajouterNote("cueille", 20.0);
+        //pas de note dans l'epluchage
+        etu1.ajouterNote("cuisson", 0.0);
+        etu1.ajouterNote("cuisson", 10.0);
+        double exp = 10.0;
+        //methode a tester
+        double res = etu1.calculerMoyenneGenerale();
+        //assertion
+        assertEquals("l'etudiant doit avoir la bonne moyenne", exp, res, 0.01);
+    }
+
+    /**
+     * test qui verifie que si il n'y a pas de note on renvoie une exception
+     * @throws ListeNotesVideException exception attendue
+     */
+    @Test (expected = ListeNotesVideException.class)
+    public void calculerMoyenneGenerale_casPasNote() throws ListeNotesVideException{
+        //preparation
+        //methode a tester
+        etu1.calculerMoyenneGenerale();
+        //assertion
+    }
+
+    /**
+     * test qui permet de verifier que calculer moyenne generale traite correctement le cas ou il y a une infinite de chiffre apres la virgule
+     * @throws MatiereInexistanteException exception non attendue
+     * @throws ValeurImpossibleException exception non attendue
+     * @throws ListeNotesVideException exception non attendue
+     */
+    @Test
+    public void calculerMoyenneGenerale_casMoyenneAvecVirguleInfinie() throws MatiereInexistanteException, ValeurImpossibleException, ListeNotesVideException {
+        //preparation
+        etu1.ajouterNote("cueille", 1.75);
+        etu1.ajouterNote("epluchage", 0.5);
+        //pas de note
+        double exp = 0.71;
+        //methode a tester
+        double res = etu1.calculerMoyenneGenerale();
+        //assertion
+        assertEquals("l'etudiant doit avoir la bonne moyenne", exp, res, 0.01);
+    }
+
+    /**
+     * test de equals dans les cas normaux
+     */
+    @Test
+    public void testEquals_casNormal() {
+        //mise en place
+        Formation formationMath = new Formation("Math");
+        Etudiant etu1Bis = new Etudiant(new Identite("SK1", "Souta", "Kamazu"), formationMath);
+        Etudiant etu1Diff = new Etudiant(new Identite("SK2", "Souta", "Kamazu"), formationMath);
+        //assertion
+        assertNotEquals(etu1, etu2);
+        assertEquals(etu1, etu1);
+        assertEquals(etu1Bis, etu1);
+        assertNotEquals(etu1Bis, etu1Diff);
+    }
+
+    /**
+     * test du cas ou l'une des valeurs est null pour equals
+     */
+    @Test
+    public void testEquals_casNull() {
+        assertFalse(etu1.equals(null));
+        assertNotEquals(null, etu2);
+    }
+
 }
