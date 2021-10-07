d125081359e719c18b0c7d2434654928dba41d8c
Nimarcs <moimonautremoi@gmail.com>
2021-10-07 13:49:19 +0200

    Initialisation de TestsGroupe

---
+++ src/tests/TestsGroupe.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * classe pour tester les methodes de la classe Groupe
+ */
+public class TestsGroupe {
+
+
+
+}
